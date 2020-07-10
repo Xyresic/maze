ec68daa3c76d21d0d11eb0c0ca9800edffa130fb
Eric Lam <elam00@stuy.edu>
2020-07-09 20:04:08 -0400

    completed Kruskal's algorithm

--- main.cpp
+++ main.cpp
@@ -1,122 +1,132 @@
-#include "Cell.h"
-
 #include <iostream>
-#include <stack>
+#include <vector>
 #include <algorithm>
-#include <chrono>
+#include <numeric>
 #include <random>
-#include <array>
+#include <chrono>
+#include <string>
 
+template <class generic>
+auto find (std::vector<generic>& vec, generic val) {
+    return std::find(vec.begin(), vec.end(), val);
+}
 
-void addPath(Cell arr[][10], std::stack<Cell*> &stackRef, std::default_random_engine &engine) {
-    Cell* currentCellPtr = stackRef.top();
-    stackRef.pop();
-
-    //shuffle array of cardinal directions
-    std::array<char, 4> directions = {'N', 'S', 'E', 'W'};
-    std::shuffle(directions.begin(), directions.end(), engine);
-
-    //ask neighbors based on shuffled array
-    for (int i = 0; i < 4; i++) {
-        switch (directions[i]) {
-            case 'N':
-                if (currentCellPtr->y > 0) {
-                    if (!arr[currentCellPtr->y - 1][currentCellPtr->x].visited) {
-                        stackRef.push(currentCellPtr);
-                        currentCellPtr->northWall = false;
-                        arr[currentCellPtr->y - 1][currentCellPtr->x].southWall = false;
-                        arr[currentCellPtr->y - 1][currentCellPtr->x].visited = true;
-                        stackRef.push(&arr[currentCellPtr->y - 1][currentCellPtr->x]);
-
-                        break;
-                    }
-                }
-
-            case 'S':
-                if (currentCellPtr->y < 9) {
-                    if (!arr[currentCellPtr->y + 1][currentCellPtr->x].visited) {
-                        stackRef.push(currentCellPtr);
-                        currentCellPtr->southWall = false;
-                        arr[currentCellPtr->y + 1][currentCellPtr->x].northWall = false;
-                        arr[currentCellPtr->y + 1][currentCellPtr->x].visited = true;
-                        stackRef.push(&arr[currentCellPtr->y + 1][currentCellPtr->x]);
-
-                        break;
-                    }
-                }
-
-            case 'E':
-                if (currentCellPtr->x < 9) {
-                    if (!arr[currentCellPtr->y][currentCellPtr->x + 1].visited) {
-                        stackRef.push(currentCellPtr);
-                        currentCellPtr->eastWall = false;
-                        arr[currentCellPtr->y][currentCellPtr->x + 1].westWall = false;
-                        arr[currentCellPtr->y][currentCellPtr->x + 1].visited = true;
-                        stackRef.push(&arr[currentCellPtr->y][currentCellPtr->x + 1]);
-
-                        break;
-                    }
-                }
-
-            case 'W':
-                if (currentCellPtr->x > 0) {
-                    if (!arr[currentCellPtr->y][currentCellPtr->x - 1].visited) {
-                        stackRef.push(currentCellPtr);
-                        currentCellPtr->westWall = false;
-                        arr[currentCellPtr->y][currentCellPtr->x - 1].eastWall = false;
-                        arr[currentCellPtr->y][currentCellPtr->x - 1].visited = true;
-                        stackRef.push(&arr[currentCellPtr->y][currentCellPtr->x - 1]);
-
-                        break;
-                    }
-                }
+void get_trees(int cell1, int cell2, int* cell1_tree, int* cell2_tree, std::vector<std::vector<int>>& forest) {
+    for (int i = 0; i < forest.size(); i++) {
+        auto end = forest[i].end();
+        if (find(forest[i], cell1) != end) {
+            *cell1_tree = i;
+        }
+        if (find(forest[i], cell2) != end) {
+            *cell2_tree = i;
         }
     }
 }
 
-void printMaze(Cell arr[][10]) {
-    std::string roof = " ";
-    for (int i = 0; i < 10 * 2 - 1; i++) {
-        roof += "_";
+void merge_trees(int index1, int index2, std::vector<std::vector<int>>& forest) {
+    for (int i : forest[index2]) {
+        forest[index1].push_back(i);
     }
-    std::cout << roof << std::endl;
+    forest.erase(forest.begin() + index2);
+}
 
-    std::string rowString;
-    for (int row = 0; row < 10; row++) {
-        rowString += "|";
-        for (int col = 0; col < 10; col++) {
-            arr[row][col].southWall ? rowString += "_" : rowString += " ";
-            if (arr[row][col].eastWall) {
-                rowString += "|";
-            } else {
-                arr[row][col].southWall && arr[row][col + 1].southWall ? rowString += "_" : rowString += " ";
-            }
+bool check (int cell1, int cell2, std::vector<std::vector<int>>& forest) {
+    int cell1_tree = -1, cell2_tree = -2;
+    get_trees(cell1, cell2, &cell1_tree, &cell2_tree, forest);
+    if (cell1_tree != -1) {
+        if (cell2_tree == -2) {
+            forest[cell1_tree].push_back(cell2);
+        } else if (cell1_tree != cell2_tree) {
+            merge_trees(cell1_tree, cell2_tree, forest);
         }
-
-        std::cout << rowString << std::endl;
-        rowString = "";
+    } else if (cell2_tree != -2) {
+        forest[cell2_tree].push_back(cell1);
+    } else {
+        std::vector<int> new_tree = {cell1, cell2};
+        forest.push_back(new_tree);
     }
+    return cell1_tree != cell2_tree;
 }
 
 int main() {
-    Cell cellGrid[10][10];
-    for (int row = 0; row < 10; row++) {
-        for (int col = 0; col < 10; col++) {
-            cellGrid[row][col].x = col;
-            cellGrid[row][col].y = row;
+    int height, width;
+    std::cout << "Enter a height: ";
+    std::cin >> height;
+    if (height < 1) {
+        std::cout << "Invalid value.";
+        abort();
+    }
+    std::cout << "Enter a width: ";
+    std::cin >> width;
+    if (width < 1) {
+        std::cout << "Invalid value.";
+        abort();
+    }
+    std::cout << "Generating a " << height << " by " << width << " maze" << std::endl;
+
+    const int area = height * width;
+    const int h_offset = height * (width - 1);
+    std::vector<std::vector<int>> mst;
+    std::vector<int> destroyed_walls;
+    std::vector<int> walls(2 * area - height - width);
+    std::iota(walls.begin(), walls.end(), 0);
+
+    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::default_random_engine engine(seed);
+    while(destroyed_walls.size() < height * width - 1) {
+        std::uniform_int_distribution<int> distribution(0, walls.size()-1);
+        int index = distribution(engine);
+        int wall = walls[index];
+        if (wall < h_offset) {
+            int offset = wall / (width - 1);
+            if (check(wall + offset, wall + offset + 1,mst)) {
+                destroyed_walls.push_back(wall);
+            }
+        } else {
+            int above = wall - h_offset;
+            if(check(above, above + width, mst)) {
+                destroyed_walls.push_back(wall);
+            }
+        }
+        walls.erase(walls.begin() + index);
+    }
+
+    std::vector<std::string> maze;
+
+    std::string horizontals;
+    for (int i = 0; i < width; i++) {
+        horizontals += "+---";
+    }
+    horizontals += "+\n";
+
+    std::string verticals;
+    for (int i = 0; i < width; i++) {
+        verticals += "|   ";
+    }
+    verticals += "|\n";
+
+    for (int i = 0; i < 2 * height + 1; i++) {
+        if (i % 2 == 0) {
+            maze.push_back(horizontals);
+        } else {
+            maze.push_back(verticals);
+        }
+    }
+    
+    for (int wall : destroyed_walls) {
+        if (wall < h_offset) {
+            int start = 4 + wall % (width - 1) * 4;
+            maze[wall / (width - 1) * 2 + 1].replace(start, 1, " ");
+        } else {
+            int start = 1 + (wall - h_offset) % width * 4;
+            maze[(wall - h_offset) / width * 2 + 2].replace(start, 3, "   ");
         }
     }
 
-    std::stack<Cell*> cellPtrStack;
-    cellPtrStack.push(&cellGrid[0][0]); //starter cell
-    cellGrid[0][0].visited = true;
+    maze[1].replace(0, 1, " ");
+    maze[2 * height - 1].replace(4 * width, 1, " ");
 
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
-
-    while (!cellPtrStack.empty()) {
-        addPath(cellGrid, cellPtrStack, engine);
+    for (auto& row : maze) {
+        std::cout << row;
     }
-
-    printMaze(cellGrid);
 }
